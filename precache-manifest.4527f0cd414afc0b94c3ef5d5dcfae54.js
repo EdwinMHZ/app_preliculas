287e3e0a339ca476697a634baf05b37656fc0e00
Edwin Morales <edwin.morales.hdz@gmail.com>
2020-10-12 20:13:38 -0500

    Updates

---
+++ precache-manifest.4527f0cd414afc0b94c3ef5d5dcfae54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9db8ff5b49230ed18d11c5206680f015",
+    "url": "/app_peliculas/index.html"
+  },
+  {
+    "revision": "bd4efebda3afe1b0e2d6",
+    "url": "/app_peliculas/static/css/2.62a1ab66.chunk.css"
+  },
+  {
+    "revision": "bd4efebda3afe1b0e2d6",
+    "url": "/app_peliculas/static/js/2.af3bb55a.chunk.js"
+  },
+  {
+    "revision": "a3c66bc87fa121efed0250fdac2dfa2b",
+    "url": "/app_peliculas/static/js/2.af3bb55a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9714965634f96d12f7d",
+    "url": "/app_peliculas/static/js/main.d6600d6e.chunk.js"
+  },
+  {
+    "revision": "6da828b58d0e28329d68",
+    "url": "/app_peliculas/static/js/runtime-main.61b346c9.js"
+  }
+]);
